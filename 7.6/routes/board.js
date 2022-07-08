808f28b39b42391054614a921b4b1fdeaab35b83
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-07-08 17:53:49 +0900

    7.8 crud study
    < location href > data transfer

--- 7.6/routes/board.js
+++ 7.6/routes/board.js
@@ -49,5 +49,76 @@ router.get("/writing", function(req, res){
         }
     );
 })
+//선택한 게시물 로드
+router.get("/info", function(req, res){
+    let no = req.query._no;
+    
+    connection.query(
+        `select * from board where No = ?`,[no],
+        function(err, result){
+            if(err){
+                console.log("글 불러오기 에러: "+err);
+            }else {
+                res.render("info.ejs", {info : result}); //받아온 result를 다음 페이지로 건네줄 준비를 해야지!
+            }
+        }
+    )
+})
+
+//해당 게시물 수정페이지로 이동
+router.get("/update", function(req, res){
+    let no = req.query._no;
+    let title = req.query._title;
+    let contents = req.query._contents;
+    let writer = req.query._writer;
+
+    res.render("update.ejs",{
+        u_no : no, 
+        u_title : title, 
+        u_contents : contents, 
+        u_writer : writer
+        }
+    )
+})
+
+//해당 게시물 수정
+router.post("/update2", function(req, res){
+    let no = req.body._no;
+    let title = req.body._title;
+    let writer = req.body._writer;
+    let contents = req.body._contents;
+    console.log(no, title, writer, contents+"를 수정할거죠?")
+
+    connection.query(
+        `update board set title = ? , writer = ? , contents = ? where No = ?`,[title, writer, contents, no],
+        function(err, result){
+            if(err){
+                console.log('게시글 수정 에러: '+err);
+            }else{
+                console.log('성공적으로 게시글을 수정했습니다');
+                res.redirect("/board/info?_no="+no);
+            }
+        }
+    )
+})
+
+
+//해당 게시물 삭제
+router.get("/delete", function(req, res){
+    let no = req.query._no;
+    console.log("삭제할 글의 No는 "+no+"입니다");
+
+    connection.query(
+        `delete from board where No = ?`,[no],
+        function(err, result){
+            if(err){
+                console.log("글 삭제 에러: "+err);
+            }else{
+                console.log('글이 성공적으로 삭제 되었습니다.')
+                res.redirect("/board");//이거의 기준은 index.js (첫 페이지)이다!
+            }
+        }
+    )
+})
 
 module.exports = router;
